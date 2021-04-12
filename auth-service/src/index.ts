2633d1af62d18b4bf994c1e6e52f2d1fec5689f5
Grzegorz <grzegorz.sobczyk125@gmail.com>
2021-04-12 02:35:31 +0200

    Create simple routes, revert changes from before using gcloud

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -1,12 +1,12 @@
 import express, {json} from 'express';
+import {currentUserRouter} from './routes/current-user';
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there');
-});
+app.use(currentUserRouter);
 
 app.listen(3000, () => {
     console.log('port test again test');
+    console.log('another');
 });
