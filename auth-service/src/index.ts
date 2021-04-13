b6bbef98fdf21f08ca030cebaa354ddc12744023
Grzegorz <grzegorz.sobczyk125@gmail.com>
2021-04-13 22:17:56 +0200

    Add user model

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -1,5 +1,8 @@
 import express, {json} from 'express';
 import 'express-async-errors';
+import mongoose from 'mongoose';
+
+
 import {currentUserRouter} from './routes/current-user';
 import {signInRouter} from './routes/signin';
 import {signOutRouter} from './routes/signout';
@@ -19,8 +22,20 @@ app.get('*', async (req, res) => {
 });
 app.use(errorHandler);
 
-
-app.listen(3000, () => {
-    console.log('port test again test');
-    console.log('another');
-});
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('connecting to mongodb');
+    } catch (error) {
+        console.log(error);
+    }
+    app.listen(3000, () => {
+        console.log('port test again test');
+        console.log('another');
+    });
+};
+start();
