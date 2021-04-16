0cca08dabfb7268bf0463b6aa4c3c9e905406950
Grzegorz <grzegorz.sobczyk125@gmail.com>
2021-04-16 19:40:41 +0200

    Add signin logic, create basic current user template

--- auth-service/src/routes/signin.ts
+++ auth-service/src/routes/signin.ts
@@ -1,9 +1,43 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import {body} from 'express-validator';
+import {validateRequest} from '../middlewares/validate-request';
+import {User} from '../models/user';
+import {BadRequestError} from '../errors/bad-request-error';
+import {PasswordManager} from '../services/password-manager';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('Hi there 123');
-});
+router.post('/api/users/signin',
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Invalid email'),
+        body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('You must supply password')
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const {email, password} = req.body;
+        const existingUser = await User.findOne({email});
+        if (!existingUser) {
+            throw new BadRequestError('Invalid credentials');
+        }
+        const isLoggedIn = await PasswordManager.compare(existingUser.password, password);
+        if (!isLoggedIn) {
+            throw new BadRequestError('Invalid credentials');
+        }
+
+        const userJWT = jwt.sign({
+            id: existingUser.id,
+            email: existingUser.email
+        }, process.env.JWT_KEY!);
+        req.session = {
+            jwt: userJWT
+        };
+        res.status(200).send(existingUser);
+    });
 
 export {router as signInRouter};
