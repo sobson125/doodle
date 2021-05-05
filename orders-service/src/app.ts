a7b7f63c71a2291ffb048318952f98dc9109d581
Grzegorz <grzegorz.sobczyk125@gmail.com>
2021-05-05 16:10:44 +0200

    Setup orders service

---
+++ orders-service/src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import {currentUser, errorHandler, NotFoundError} from '@sobsontickets/common';
+
+
+const app = express();
+
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+}));
+app.use(currentUser);
+
+app.get('*', async (req, res) => {
+    throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export {app};
